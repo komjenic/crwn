55e1c4828414c434caf808c605b0c4294231a343
Ivan Komjenic <komjenic@gmail.com>
2019-09-03 20:39:38 +0200

    added addItem and removeItem functionality to checkout item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,7 +6,7 @@ import {
     selectCartItems,
     selectCartTotal
 } from '../../redux/cart/cart.selectors';
-import CheckoutItem from '../../components/checkout-item/checkout-item..component';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import './checkout.styles.scss';
 
