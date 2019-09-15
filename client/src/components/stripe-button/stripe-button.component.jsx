de545f22727d22b35872fa8c65a66ad96eb74e8e
Ivan Komjenic <komjenic@gmail.com>
2019-09-15 19:45:44 +0200

    separating our crwn app into client folder, added express server to help backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_Pjo0QnXIoqSEw2yI8fOFBe6e00Cw5WwGc4';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token: token
+            }
+        })
+            .then(response => {
+                alert('succesful payment');
+            })
+            .catch(error => {
+                console.log('Payment Error: ', JSON.parse(error));
+                alert(
+                    'There was an issue with your payment! Please make sure you use the provided credit card.'
+                );
+            });
     };
 
     return (
         <StripeCheckout
             label='Pay Now'
-            name='CRWN Cloathing Ltd.'
+            name='CRWN Clothing Ltd.'
             billingAddress
             shippingAddress
             image='https://svgshare.com/i/CUz.svg'
